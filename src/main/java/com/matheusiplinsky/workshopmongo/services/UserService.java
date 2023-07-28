6a2ac9e52fa14e2027a0abb5cbfbb35d464311df
Matheus Iplinsky <matheusiplinsky05@gmail.com>
2023-07-28 16:07:41 -0300

    Inserção de usuário com POST

--- src/main/java/com/matheusiplinsky/workshopmongo/services/UserService.java
+++ src/main/java/com/matheusiplinsky/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.matheusiplinsky.workshopmongo.DTO.UserDTO;
 import com.matheusiplinsky.workshopmongo.domain.User;
 import com.matheusiplinsky.workshopmongo.repository.UserRepository;
 import com.matheusiplinsky.workshopmongo.services.exception.ObjectNotFoundException;
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
